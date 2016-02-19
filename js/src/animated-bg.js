92958da121fb0c7b4db63c4f7abd9ff0a52de865
James Harris <computersherpa@gmail.com>
2016-02-18 21:31:40 -0600

    Set up ES6 transpiler

--- js/src/animated-bg.js
+++ js/src/animated-bg.js
@@ -1,20 +1,20 @@
 document.getElementById('animated-bg').width = window.innerWidth;
 
-var animatedBg = function () {
+var animatedBg = (function() {
 	var exports = {};
-	exports.initCanvas = function () {
+	exports.initCanvas = (function() {
 		exports.stage = new createjs.Stage('animated-bg');
 		exports.stage.update();
-	}();
-	exports.updateCanvas = function () {
+	})();
+	exports.updateCanvas = function() {
 		exports.stage.update();
 	};
-	exports.updateCanvasWidth = function () {
+	exports.updateCanvasWidth = function() {
 		document.getElementById('animated-bg').width = window.innerWidth;
 		exports.updateCanvas();
 	};
 	window.addEventListener('resize', exports.updateCanvasWidth);
-	exports.testCanvas = function () {
+	exports.testCanvas = function() {
 		var circle = new createjs.Shape();
 		circle.graphics.beginFill("red").drawCircle(0, 0, 40);
 		circle.x = circle.y = 50;
@@ -29,12 +29,10 @@ var animatedBg = function () {
 		var shape = new createjs.Shape();
 		shape.graphics.setStrokeStyle("8").beginStroke("silver").beginFill("white");
 		var r = Math.random();
-		if (r > 0.6) {
-			// circle
+		if (r > 0.6) { // circle
 			shape.graphics.drawCircle(0, 0, radius);
-		} else if (r > 0.3) {
-			// square
-			shape.graphics.drawRect(-radius, -radius, radius * 2, radius * 2);
+		} else if (r > 0.3) { // square
+			shape.graphics.drawRect(-radius, -radius, radius*2, radius*2);
 		} else {
 			shape.graphics.drawPolyStar(0, 0, radius, 3, 0, r * 360);
 		}
@@ -43,14 +41,14 @@ var animatedBg = function () {
 		exports.stage.addChild(shape);
 		exports.stage.update();
 	};
-	exports.createRandomShapes = function (minX, maxX, minY, maxY, quantity) {
+	exports.createRandomShapes = function(minX, maxX, minY, maxY, quantity) {
 		var x, y;
 		for (var i = 0; i < quantity; i++) {
-			x = (maxX - minX) * Math.random() + minX;
-			y = (maxY - minY) * Math.random() + minY;
+			x = ((maxX - minX) * Math.random()) + minX;
+			y = ((maxY - minY) * Math.random()) + minY;
 			exports.createRandomShape(x, y);
 		}
 	};
 	exports.createRandomShapes(300, 1000, 100, 300, 50);
 	return exports;
-}();
+})();
